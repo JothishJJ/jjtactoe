ae5d29b77148570b363fa1842b0c39b2eae880c5
Jothish JJ <jothishjj11@gmail.com>
2022-11-15 15:03:38 +0000

    Add mdb bootstrap

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -2,13 +2,18 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
-  template: `<button>{{ value }}</button>`,
+  template: `<button class="btn btn-success" *ngIf="value == 'X'">
+      {{ value }}
+    </button>
+    <button class="btn btn-primary" *ngIf="value == 'O'">{{ value }}</button>
+    <button class="btn btn-dark" *ngIf="!value">{{ value }}</button> `,
   styles: [
     `
       button {
         height: 200px;
         width: 200px;
-        font-size: 200px;
+        font-size: 100px;
+        border: 4px black solid;
       }
     `,
   ],
